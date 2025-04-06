f989cc31e3f67b97217a99d8892f7b0748d7b5b4
User5842 <r.negron1@umiami.edu>
2025-04-06 09:22:03 -0400

    adding new styling; social links; icons for external links; modifying social graph meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: { type: "text", value: " 🔗" },
+        },
+      ],
+    ],
     remarkPlugins: [remarkReadingTime],
   },
   vite: {
