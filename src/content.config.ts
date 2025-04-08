ab5d9054ae15f223fb46bfc341b031141c2062a4
User5842 <r.negron1@umiami.edu>
2025-04-07 21:18:21 -0400

    adding blog section

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,13 +2,20 @@ import { defineCollection, z } from "astro:content";
 
 import { glob } from "astro/loaders";
 
-const guide = defineCollection({
-  loader: glob({ base: "./guide", pattern: "**/*.md" }),
-  schema: z.object({
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    title: z.string(),
-  }),
+const schema = z.object({
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  title: z.string(),
 });
 
-export const collections = { guide };
+const blog = defineCollection({
+  loader: glob({ base: "./blog", pattern: "**/*.md" }),
+  schema,
+});
+
+const guide = defineCollection({
+  loader: glob({ base: "./guide", pattern: "**/*.md" }),
+  schema,
+});
+
+export const collections = { blog, guide };
