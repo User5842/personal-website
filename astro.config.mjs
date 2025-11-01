3470f25564cdf435cdf70c02b2b877f8a9287a71
User5842 <negron.a.rafael@gmail.com>
2025-11-01 16:03:23 -0400

    somewhat complete with the compound interest form

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   site: "https://rafaelnegron.me/",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
