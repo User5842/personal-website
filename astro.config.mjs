257b2fd3d1714f517f4db5ec79b4fd1851b97fa2
User5842 <negron.a.rafael@gmail.com>
2025-11-01 11:43:26 -0400

    complete refresh of personal website; initial scaffold

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import rehypeExternalLinks from "rehype-external-links";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        {
-          content: { type: "text", value: " 🔗" },
-        },
-      ],
-    ],
-    remarkPlugins: [remarkReadingTime],
-  },
   site: "https://rafaelnegron.me/",
   vite: {
     plugins: [tailwindcss()],
